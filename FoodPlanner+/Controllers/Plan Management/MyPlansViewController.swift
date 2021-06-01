20773eb00a3bf77719dc96cc33c65f7ee4472549
Alastair Tooth <alastairtooth@gmail.com>
2021-06-01 16:29:13 +1000

    creating a pageview planner screen

---
+++ FoodPlanner+/Controllers/Plan Management/MyPlansViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPlansViewController.swift
+//  FoodPlanner+
+//
+//  Created by Alastair Tooth on 1/6/21.
+//
+
+import UIKit
+
+class MyPlansViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoodPlanner+/Models/NavBarSettings.swift
@@ -0,0 +1,8 @@
+//
+//  NavBarSettings.swift
+//  FoodPlanner+
+//
+//  Created by Alastair Tooth on 1/6/21.
+//
+
+import Foundation
